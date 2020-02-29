990734870281702d307bba75ce82aaef061a40d9
Gregory Rothstein <gregory@thecommodoredesigns.com>
2020-02-29 13:24:11 -0500

    importing the album interface and the product service

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Album } from '../album';
+import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-tracklisting',
   templateUrl: './product-tracklisting.component.html',
