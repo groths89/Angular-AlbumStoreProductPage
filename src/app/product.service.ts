ba15505ad84bfba8623479ac95935a80736582ef
Gregory Rothstein <gregory@thecommodoredesigns.com>
2020-02-29 11:55:33 -0500

    correcting tasks 7 and 8 with correct code

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,8 +14,8 @@ export class ProductService {
   
   constructor(private _http: Http) { }
   
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response)=>response.json() as Observable<Album>);
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
 }
 
